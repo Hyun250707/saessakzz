f2369faf10ced6044a725075897bda6de4e590b3
kas <cook99111@gmail.com>
2026-05-21 02:09:56 +0900

    환경세팅 완료 및 앱 실행

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:navi_app/main.dart';
+import 'package:saessakzz/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
